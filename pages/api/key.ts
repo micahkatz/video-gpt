712d3560f5a45be56b4d196101d4a1701e2f6757
micahkatz <micahj2110@gmail.com>
2023-09-09 15:15:13 -0500

    added functionality for saving keys

--- pages/api/key.ts
+++ pages/api/key.ts
@@ -1,39 +1,46 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import clerk, { getAuth, clerkClient } from "@clerk/nextjs/server";
+import { getAuth, clerkClient } from "@clerk/nextjs/server";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { sessionId, userId } = getAuth(req);
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+  const { userId } = getAuth(req);
+  switch (req.method) {
+    case "GET":
+      try {
+        const user = await clerkClient.users.getUser(userId);
+        return res.status(200).json(user?.privateMetadata || {});
+      } catch (err) {
+        return res.status(500).send({});
+      }
+    case "POST":
+      const openAIApiKey = req?.body?.openAIApiKey;
+      const elevenLabsApiKey = req?.body?.elevenLabsApiKey;
+      const googleSearchApiKey = req?.body?.googleSearchApiKey;
+      const cseId = req?.body?.cseId;
 
-  const {
-    openAIApiKey,
-    elevenLabsApiKey,
-    googleSearchApiKey,
-    cseId,
-  } = req.body;
+      console.log({ body: req.body });
 
-  try {
+      try {
+        const privateMetadata = {
+          OPENAI_API_KEY: openAIApiKey,
+          CSE_ID: cseId,
+          ELEVEN_LABS_API_KEY: elevenLabsApiKey,
+          GOOGLE_SEARCH_API_KEY: googleSearchApiKey,
+        };
 
-    const userId = "my-user-id";
+        console.log({ privateMetadata });
+        const updatedUser = await clerkClient.users.updateUser(userId, {
+          privateMetadata,
+        });
 
-    const privateMetadata = {
-      openAIApiKey,
-      elevenLabsApiKey,
-      googleSearchApiKey,
-      cseId,
-    };
+        console.log("API Keys and CSE ID saved for user:", updatedUser);
 
-    const updatedUser = await clerkClient.users.updateUser(userId, {
-      privateMetadata,
-    });
-
-    console.log("API Keys and CSE ID saved for user:", updatedUser);
-
-    return res.status(200).json({ message: "API keys saved successfully" });
-  } catch (error) {
-    console.error("Error saving API keys:", error);
-    return res.status(500).json({ error: "Internal server error" });
+        return res.status(200).json({ message: "API keys saved successfully" });
+      } catch (error) {
+        console.error("Error saving API keys:", error);
+        return res.status(500).json({ error: "Internal server error" });
+      }
+      break;
+    default:
+      res.status(405).json({ error: "Method not allowed" });
   }
 }
--- pages/user/apikeys.tsx
+++ pages/user/apikeys.tsx
@@ -1,13 +1,56 @@
-import React, { useState } from 'react';
-import Header from '../../components/Header';
-import { FaEye, FaEyeSlash, FaExternalLinkAlt } from 'react-icons/fa';
-import Link from 'next/link';
+import React, { useState } from "react";
+import Header from "../../components/Header";
+import { FaEye, FaEyeSlash, FaExternalLinkAlt } from "react-icons/fa";
+import Link from "next/link";
+import { useMutation, useQuery } from "react-query";
+import axios from "axios";
 
 const ApiKeys = () => {
   const [showOpenAIApiKey, setShowOpenAIApiKey] = useState(false);
   const [showElevenLabsApiKey, setShowElevenLabsApiKey] = useState(false);
   const [showGoogleSearchApiKey, setShowGoogleSearchApiKey] = useState(false);
   const [showCseId, setShowCseId] = useState(false);
+  const [openAIApiKey, setOpenAIApiKey] = useState(undefined);
+  const [elevenLabsApiKey, setElevenLabsApiKey] = useState(undefined);
+  const [googleSearchApiKey, setGoogleSearchApiKey] = useState(undefined);
+  const [cseId, setCseId] = useState(undefined);
+
+  const getKeys = async () => {
+    return axios.get("/api/key");
+  };
+
+  const apiKeysQuery = useQuery(["apiKeys"], {
+    queryFn: getKeys,
+    onSuccess: (data) => {
+      const {
+        OPENAI_API_KEY: storedOpenaiApiKey,
+        CSE_ID: storedCseId,
+        ELEVEN_LABS_API_KEY: storedElevenLabsApiKey,
+        GOOGLE_SEARCH_API_KEY: storedGoogleSearchApiKey,
+      } = data?.data || {
+        OPENAI_API_KEY: undefined,
+        CSE_ID: undefined,
+        ELEVEN_LABS_API_KEY: undefined,
+        GOOGLE_SEARCH_API_KEY: undefined,
+      };
+      setOpenAIApiKey(storedOpenaiApiKey);
+      setCseId(storedCseId);
+      setElevenLabsApiKey(storedElevenLabsApiKey);
+      setGoogleSearchApiKey(storedGoogleSearchApiKey);
+    },
+  });
+
+  const {
+    OPENAI_API_KEY: storedOpenaiApiKey,
+    CSE_ID: storedCseId,
+    ELEVEN_LABS_API_KEY: storedElevenLabsApiKey,
+    GOOGLE_SEARCH_API_KEY: storedGoogleSearchApiKey,
+  } = apiKeysQuery?.data?.data || {
+    OPENAI_API_KEY: undefined,
+    CSE_ID: undefined,
+    ELEVEN_LABS_API_KEY: undefined,
+    GOOGLE_SEARCH_API_KEY: undefined,
+  };
 
   const handleSaveKeys = async () => {
     const data = {
@@ -16,26 +59,17 @@ const ApiKeys = () => {
       googleSearchApiKey,
       cseId,
     };
-
-    try {
-      const response = await fetch('/api/key', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to save API keys');
-      }
-
-      console.log('API Keys Saved!');
-    } catch (error) {
-      console.error('Error saving API keys:', error);
-    }
+    return fetch("/api/key", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
   };
-
+  const saveKeysMutation = useMutation({
+    mutationFn: handleSaveKeys,
+  });
 
   return (
     <div className="bg-background min-h-screen flex flex-col justify-center pb-6">
@@ -47,112 +81,152 @@ const ApiKeys = () => {
           By providing your API key, you agree to be solely responsible for its security and usage. We will not disclose
           your API key to third parties without your consent.
         </p>
-
-        <div className="mb-4">
-          <Link target="_blank" href="https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key">
-            <div className="flex items-center mb-2 cursor-pointer">
-              <div className="text-xl font-semibold text-black">OpenAI API Key</div>
-              <span className="ml-2 text-gray-500">
-                <FaExternalLinkAlt />
-              </span>
-            </div>
-          </Link>
-          <div className="relative border-2 border-primary-300 rounded min-w-20">
-            <input
-              type={showOpenAIApiKey ? 'text' : 'password'}
-              placeholder="Enter OpenAI API Key"
-              className="w-full border rounded px-4 py-2 text-gray-700 bg-white"
-            />
-            <span
-              className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
-              onClick={() => setShowOpenAIApiKey(!showOpenAIApiKey)}
-            >
-              {showOpenAIApiKey ? <FaEyeSlash className="text-gray-500" /> : <FaEye className="text-gray-500" />}
-            </span>
-          </div>
-        </div>
-
-        <div className="mb-4">
-          <Link target="_blank" href="https://docs.elevenlabs.io/api-reference/quick-start/authentication">
-            <div className="flex items-center mb-2 cursor-pointer">
-              <div className="text-xl font-semibold text-black">ElevenLabs API Key</div>
-              <span className="ml-2 text-gray-500">
-                <FaExternalLinkAlt />
-              </span>
-            </div>
-          </Link>
-          <div className="relative border-2 border-primary-300 rounded min-w-20">
-            <input
-              type={showElevenLabsApiKey ? 'text' : 'password'}
-              placeholder="Enter ElevenLabs API Key"
-              className="w-full border rounded px-4 py-2 text-gray-700 bg-white"
-            />
-            <span
-              className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
-              onClick={() => setShowElevenLabsApiKey(!showElevenLabsApiKey)}
-            >
-              {showElevenLabsApiKey ? <FaEyeSlash className="text-gray-500" /> : <FaEye className="text-gray-500" />}
-            </span>
-          </div>
-        </div>
-
-        <div className="mb-4">
-          <Link target="_blank" href="https://developers.google.com/custom-search/v1/introduction">
-            <div className="flex items-center mb-2 cursor-pointer">
-              <div className="text-xl font-semibold text-black">Google Search API Key</div>
-              <span className="ml-2 text-gray-500">
-                <FaExternalLinkAlt />
-              </span>
-            </div>
-          </Link>
-          <div className="relative border-2 border-primary-300 rounded min-w-20">
-            <input
-              type={showGoogleSearchApiKey ? 'text' : 'password'}
-              placeholder="Enter Google Search API Key"
-              className="w-full border rounded px-4 py-2 text-gray-700 bg-white"
-            />
-            <span
-              className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
-              onClick={() => setShowGoogleSearchApiKey(!showGoogleSearchApiKey)}
-            >
-              {showGoogleSearchApiKey ? <FaEyeSlash className="text-gray-500" /> : <FaEye className="text-gray-500" />}
-            </span>
-          </div>
-        </div>
-
-        <div className="mb-4">
-          <Link target="_blank" href="https://developers.google.com/custom-search/v1/introduction">
-            <div className="flex items-center mb-2 cursor-pointer">
-              <div className="text-xl font-semibold text-black">CSE ID</div>
-              <span className="ml-2 text-gray-500">
-                <FaExternalLinkAlt />
-              </span>
-            </div>
-          </Link>
-          <div className="relative border-2 border-primary-300 rounded min-w-20">
-            <input
-              type={showCseId ? 'text' : 'password'}
-              placeholder="Enter CSE ID"
-              className="w-full border rounded px-4 py-2 text-gray-700 bg-white"
-            />
-            <span
-              className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
-              onClick={() => setShowCseId(!showCseId)}
-            >
-              {showCseId ? <FaEyeSlash className="text-gray-500" /> : <FaEye className="text-gray-500" />}
-            </span>
-          </div>
+        {apiKeysQuery.isLoading ? (
+          <p>Loading</p>
+        ) : (
+          <>
+            <div className="mb-4">
+              <Link
+                target="_blank"
+                href="https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key"
+              >
+                <div className="flex items-center mb-2 cursor-pointer">
+                  <div className="text-xl font-semibold text-black">OpenAI API Key</div>
+                  <span className="ml-2 text-gray-500">
+                    <FaExternalLinkAlt />
+                  </span>
+                </div>
+              </Link>
+              <div className="relative border-2 border-primary-300 rounded min-w-20 pr-6">
+                <input
+                  type={showOpenAIApiKey ? "text" : "password"}
+                  placeholder="Enter OpenAI API Key"
+                  className="w-full focus:outline-none rounded px-4 py-2 text-gray-700 bg-white"
+                  defaultValue={storedOpenaiApiKey}
+                  value={openAIApiKey}
+                  onChange={(e) => setOpenAIApiKey(e.target.value)}
+                />
+                <span
+                  className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
+                  onClick={() => setShowOpenAIApiKey(!showOpenAIApiKey)}
+                >
+                  {showOpenAIApiKey ? (
+                    <FaEyeSlash className="text-gray-500 bg-white h-full" />
+                  ) : (
+                    <FaEye className="text-gray-500 bg-white h-full" />
+                  )}
+                </span>
               </div>
-              <div className="flex justify-center">
-                  <button
-                      type="button"
-                      onClick={handleSaveKeys}
-                      className="cursor-pointer transition-all text-white bg-gradient-to-br from-secondaryAccent to-accent hover:shadow-xl hover:shadow-primary hover:scale-105 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 disabled:opacity-50"
-                  >
-                      Save Keys
-                  </button>
+            </div>
+
+            <div className="mb-4">
+              <Link target="_blank" href="https://docs.elevenlabs.io/api-reference/quick-start/authentication">
+                <div className="flex items-center mb-2 cursor-pointer">
+                  <div className="text-xl font-semibold text-black">ElevenLabs API Key (Optional)</div>
+                  <span className="ml-2 text-gray-500">
+                    <FaExternalLinkAlt />
+                  </span>
+                </div>
+              </Link>
+              <div className="relative border-2 border-primary-300 rounded min-w-20 pr-6">
+                <input
+                  type={showElevenLabsApiKey ? "text" : "password"}
+                  placeholder="Enter ElevenLabs API Key"
+                  className="w-full focus:outline-none rounded px-4 py-2 text-gray-700 bg-white"
+                  defaultValue={storedElevenLabsApiKey}
+                  value={elevenLabsApiKey}
+                  onChange={(e) => setElevenLabsApiKey(e.target.value)}
+                />
+                <span
+                  className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
+                  onClick={() => setShowElevenLabsApiKey(!showElevenLabsApiKey)}
+                >
+                  {showElevenLabsApiKey ? (
+                    <FaEyeSlash className="text-gray-500 bg-white h-full" />
+                  ) : (
+                    <FaEye className="text-gray-500 bg-white h-full" />
+                  )}
+                </span>
               </div>
-          </div>
+            </div>
+
+            <div className="mb-4">
+              <Link target="_blank" href="https://developers.google.com/custom-search/v1/introduction">
+                <div className="flex items-center mb-2 cursor-pointer">
+                  <div className="text-xl font-semibold text-black">Google Search API Key (Optional)</div>
+                  <span className="ml-2 text-gray-500">
+                    <FaExternalLinkAlt />
+                  </span>
+                </div>
+              </Link>
+              <div className="relative border-2 border-primary-300 rounded min-w-20 pr-6">
+                <input
+                  type={showGoogleSearchApiKey ? "text" : "password"}
+                  placeholder="Enter Google Search API Key"
+                  className="w-full focus:outline-none rounded px-4 py-2 text-gray-700 bg-white"
+                  defaultValue={storedGoogleSearchApiKey}
+                  value={googleSearchApiKey}
+                  onChange={(e) => setGoogleSearchApiKey(e.target.value)}
+                />
+                <span
+                  className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
+                  onClick={() => setShowGoogleSearchApiKey(!showGoogleSearchApiKey)}
+                >
+                  {showGoogleSearchApiKey ? (
+                    <FaEyeSlash className="text-gray-500 bg-white h-full" />
+                  ) : (
+                    <FaEye className="text-gray-500 bg-white h-full" />
+                  )}
+                </span>
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <Link target="_blank" href="https://developers.google.com/custom-search/v1/introduction">
+                <div className="flex items-center mb-2 cursor-pointer">
+                  <div className="text-xl font-semibold text-black">CSE ID (Optional)</div>
+                  <span className="ml-2 text-gray-500">
+                    <FaExternalLinkAlt />
+                  </span>
+                </div>
+              </Link>
+              <div className="relative border-2 border-primary-300 rounded min-w-20 pr-6">
+                <input
+                  type={showCseId ? "text" : "password"}
+                  placeholder="Enter CSE ID"
+                  className="w-full focus:outline-none rounded px-4 py-2 text-gray-700 bg-white"
+                  defaultValue={storedCseId}
+                  value={cseId}
+                  onChange={(e) => setCseId(e.target.value)}
+                />
+                <span
+                  className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
+                  onClick={() => setShowCseId(!showCseId)}
+                >
+                  {showCseId ? (
+                    <FaEyeSlash className="text-gray-500 bg-white h-full" />
+                  ) : (
+                    <FaEye className="text-gray-500 bg-white h-full" />
+                  )}
+                </span>
+              </div>
+            </div>
+          </>
+        )}
+        <div className="flex flex-col items-center gap-4">
+          <button
+            type="button"
+            onClick={() => saveKeysMutation.mutate()}
+            disabled={saveKeysMutation.isLoading}
+            className="cursor-pointer transition-all text-white bg-gradient-to-br from-secondaryAccent to-accent hover:shadow-xl hover:shadow-primary hover:scale-105 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 disabled:opacity-50"
+          >
+            Save Keys
+          </button>
+          {saveKeysMutation.isSuccess && <p className="text-green-500">Successfully Saved Keys!</p>}
+          {saveKeysMutation.isError && <p className="text-red-500">There was an error saving keys</p>}
+          {saveKeysMutation.isLoading && <p className="text-gray-500">Saving...</p>}
+        </div>
+      </div>
     </div>
   );
 };
